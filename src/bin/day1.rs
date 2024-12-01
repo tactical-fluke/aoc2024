c32759fb6d69e496b04fabad5246872ec23cd3c7
Aidan Haile <tacticalfluke304@gmail.com>
2024-12-01 19:20:59 +0800

    improve harness

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,55 +1,45 @@
-use std::fs;
-use aoc2024::day::Day;
+use aoc2024::day::{run_day, Day};
 
 fn main() {
-    println!("Hello, world!");
-
-    println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
-    let input = fs::read_to_string("inputs/day1.txt").unwrap();
-    println!("{}", Day1::solve(&input));
-    println!("{}", Day1Part2::solve(&input));
-}
-
-fn parse_day1_input(input: &str) -> (Vec<u32>, Vec<u32>) {
-    let ids: Vec<(u32, u32)> = input.lines()
-        .map(|line| line.split("   ").collect::<Vec<&str>>())
-        .map(|parts| (parts[0], parts[1]))
-        .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
-        .collect();
-
-    let mut list_1: Vec<u32> = Vec::with_capacity(ids.len());
-    let mut list_2: Vec<u32> = Vec::with_capacity(ids.len());
-    for (a, b) in ids {
-        list_1.push(a);
-        list_2.push(b);
-    }
-
-    list_1.sort();
-    list_2.sort();
-    (list_1, list_2)
+    let results = run_day::<Day1>();
+    println!("part1:{}, part2:{}", results.0, results.1);
 }
 
 struct Day1 {}
 
-impl Day<u32> for Day1 {
-    fn solve(input: &str) -> u32 {
-        let (list_1, list_2) = parse_day1_input(input);
+impl Day for Day1 {
+    const INPUT_FILE: &'static str = "day1.txt";
+    type OutputType = u32;
+    type ParsedType = (Vec<u32>, Vec<u32>);
 
+    fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
+        let ids: Vec<(u32, u32)> = input.lines()
+            .map(|line| line.split("   ").collect::<Vec<&str>>())
+            .map(|parts| (parts[0], parts[1]))
+            .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
+            .collect();
+
+        let mut list_1: Vec<u32> = Vec::with_capacity(ids.len());
+        let mut list_2: Vec<u32> = Vec::with_capacity(ids.len());
+        for (a, b) in ids {
+            list_1.push(a);
+            list_2.push(b);
+        }
+
+        list_1.sort();
+        list_2.sort();
+        (list_1, list_2)
+    }
+
+    fn part1(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+        let (list_1, list_2) = input;
         list_1.iter().zip(list_2.iter()).map(|(a, b)| u32::abs_diff(*a, *b)).sum()
     }
-}
 
-impl Day1 {}
-
-struct Day1Part2 {}
-
-impl Day<u32> for Day1Part2 {
-    fn solve(input: &str) -> u32 {
-        let (list1, list2) = parse_day1_input(input);
-
-        list1.iter()
-            .map(|item| item * list2.iter().filter(|item2| &item == item2).count() as u32)
+    fn part2(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+        let (list_1, list_2) = input;
+        list_1.iter()
+            .map(|item| item * list_2.iter().filter(|item2| &item == item2).count() as u32)
             .sum()
-
     }
 }
--- src/day.rs
+++ src/day.rs
@@ -1,3 +1,17 @@
-pub trait Day<T> {
-    fn solve(input: &str) -> T;
+use std::fs;
+
+pub trait Day {
+    const INPUT_FILE: &'static str;
+    type OutputType;
+    type ParsedType;
+
+    fn parse_input(input: &str) -> Self::ParsedType;
+    fn part1(input: &Self::ParsedType) -> Self::OutputType;
+    fn part2(input: &Self::ParsedType) -> Self::OutputType;
+}
+
+pub fn run_day<D: Day>() -> (D::OutputType, D::OutputType) {
+    let input = fs::read_to_string(format!("inputs/{}", D::INPUT_FILE)).unwrap();
+    let input = D::parse_input(&input);
+    (D::part1(&input), D::part2(&input))
 }
