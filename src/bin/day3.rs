c9052da9e3c5b1f97738ae2610b05040d46f8b6f
Aidan Haile <tacticalfluke304@gmail.com>
2024-12-03 19:09:15 +0800

    Day3 Part 1 complete

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use aoc2024::day::{run_day, Day};
 
 fn main() {
@@ -13,23 +14,33 @@ impl Day for Day3 {
     type ParsedType = Vec<(u32, u32)>;
 
     fn parse_input(input: &str) -> Self::ParsedType {
-        todo!()
+        let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+        regex.captures_iter(input)
+            .map(|caps| {
+                let (_, [val1, val2]) = caps.extract();
+                (val1.parse().unwrap(), val2.parse().unwrap())
+            }).collect()
     }
 
     fn part1(input: &Self::ParsedType) -> Self::OutputType {
-        todo!()
+        input.iter().map(|(val1, val2)| val1 * val2).sum()
     }
 
     fn part2(input: &Self::ParsedType) -> Self::OutputType {
-        todo!()
+       0
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const TEST_STRING: &str = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
     #[test]
     fn test_part1() {
+        let input = Day3::parse_input(TEST_STRING);
+        assert_eq!(Day3::part1(&input), 161);
     }
 
     #[test]
