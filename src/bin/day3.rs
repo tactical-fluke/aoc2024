9ce078d91807188b8ab02bfd534790a0e81d8308
Aidan Haile <tacticalfluke304@gmail.com>
2024-12-04 18:52:29 +0800

    small day 3 extra whitespace cleanup

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -64,8 +64,6 @@ impl Day for Day3 {
 mod tests {
     use super::*;
 
-
-
     #[test]
     fn test_part1() {
         const TEST_STRING: &str = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
