bde6dcac80c75751de142da6f9e9c76e37eb78ba
Aidan Haile <tacticalfluke304@gmail.com>
2024-12-07 14:21:04 +0800

    Add day 7 skeleton

---
+++ src/bin/day7.rs
@@ -0,0 +1,36 @@
+use aoc2024::day::{run_day, Day};
+
+fn main() {
+    let results = run_day::<Day6>();
+    //println!("part1:{}, part2:{}", results.0, results.1);
+}
+
+struct Day6 {}
+
+impl Day for Day6 {
+    const INPUT_FILE: &'static str = "day7.txt";
+    type OutputType = ();
+    type ParsedType = ();
+
+    fn parse_input(input: &str) -> Self::ParsedType {
+    }
+
+    fn part1(input: &Self::ParsedType) -> Self::OutputType {
+    }
+
+    fn part2(input: &Self::ParsedType) -> Self::OutputType {
+    }
+}
+
+#[cfg(test)]
+mod day6_tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+    }
+
+    #[test]
+    fn test_part2() {
+    }
+}
